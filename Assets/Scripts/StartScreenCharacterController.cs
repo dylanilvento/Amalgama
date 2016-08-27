641990dde12aed104ca60b9d813cdca2a7526611
Dylan Ilvento <dylanilvento@gmail.com>
2016-08-27 14:06:05 -0400

    Start screen finished besides spawning and title card.

---
+++ Assets/Scripts/StartScreenCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenCharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")) {
+			  transform.Translate(Vector2.up * Time.deltaTime * 60);
+		}
+
+		if (Input.GetKey("down")) {
+			  transform.Translate(Vector2.down * Time.deltaTime * 60);
+		}
+
+		if (Input.GetKey("left")) {
+			  transform.Translate(Vector2.left * Time.deltaTime * 60);
+		}
+
+		if (Input.GetKey("right")) {
+			  transform.Translate(Vector2.right * Time.deltaTime * 60);
+		}
+	}
+}
---
+++ Assets/Scripts/StartScreenSpawnTriggers.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenSpawnTriggers : MonoBehaviour {
+	public GameObject elementText;
+	SpriteRenderer textSR;
+	bool collided;
+	// Use this for initialization
+	void Start () {
+		textSR = elementText.GetComponent<SpriteRenderer>();
+		textSR.color = Color.clear;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.layer == 8) {
+			print("works");
+			collided = true;
+			StartCoroutine("BlinkText");
+			
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.layer == 8) {
+			// print("works");
+			collided = false;
+			// StartCoroutine("BlinkText");
+			
+		}
+	}
+
+	IEnumerator BlinkText () {
+		while (collided) {
+			textSR.color = Color.white;
+			yield return new WaitForSeconds(0.5f);	
+			textSR.color = Color.clear;
+			yield return new WaitForSeconds(0.5f);	
+		}
+		
+	}
+	
+
+}
